72b94c04348445bd63f76fbec08ea14df60b48f3
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-24 15:09:13 -0300

    add attribute fields to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,42 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <label htmlFor="name">
-        Nome
-        <input
-          type="text"
-          data-testid="name-input"
-        />
-      </label>
+      <div className="form-container">
+        <label htmlFor="name">
+          Nome
+          <input
+            type="text"
+            data-testid="name-input"
+          />
+        </label>
+        <label htmlFor="description">
+          Descrição
+          <textarea
+            data-testid="description-input"
+          />
+        </label>
+        <label htmlFor="attr1">
+          Atributo1
+          <input
+            type="text"
+            data-testid="attr1-input"
+          />
+        </label>
+        <label htmlFor="attr2">
+          Atributo1
+          <input
+            type="text"
+            data-testid="attr2-input"
+          />
+        </label>
+        <label htmlFor="attr3">
+          Atributo1
+          <input
+            type="text"
+            data-testid="attr3-input"
+          />
+        </label>
+      </div>
     );
   }
 }
