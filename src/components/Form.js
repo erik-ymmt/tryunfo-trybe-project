cb5e5b6f9409a89967dcf814f82e17c9ce183f8a
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-24 15:32:37 -0300

    add complete form - Requirement 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,24 +20,52 @@ class Form extends React.Component {
         <label htmlFor="attr1">
           Atributo1
           <input
-            type="text"
+            type="number"
             data-testid="attr1-input"
           />
         </label>
         <label htmlFor="attr2">
-          Atributo1
+          Atributo2
           <input
-            type="text"
+            type="number"
             data-testid="attr2-input"
           />
         </label>
         <label htmlFor="attr3">
-          Atributo1
+          Atributo3
           <input
-            type="text"
+            type="number"
             data-testid="attr3-input"
           />
         </label>
+        <label htmlFor="img">
+          URL da Imagem
+          <input
+            type="text"
+            data-testid="image-input"
+          />
+        </label>
+        <label htmlFor="rarity">
+          Raridade
+          <select data-testid="rare-input" id="rarity">
+            <option value="normal"> normal </option>
+            <option value="raro"> raro </option>
+            <option value="muito raro"> muito raro </option>
+          </select>
+        </label>
+        <label htmlFor="super-trunfo">
+          Super Trunfo
+          <input
+            type="checkbox"
+            data-testid="trunfo-input"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="save-button"
+        >
+          Salvar
+        </button>
       </div>
     );
   }
